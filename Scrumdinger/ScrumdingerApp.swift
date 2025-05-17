f148b044db7880828cd047018b24673de96c659e
Felix Htoo <felixhtoo@FHs-MacPro-M3.local>
2025-05-18 03:53:03 +0700

    Initial commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Felix Htoo on 17/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}

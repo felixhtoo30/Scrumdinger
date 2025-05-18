785628a6768454f8983676e2023f6efbcbe95938
Felix Htoo <felixhtoo@FHs-MacPro-M3.local>
2025-05-18 18:55:12 +0700

    wip: Initially added the CardView and DailyScrum Model

---
+++ Scrumdinger/CardView.swift
@@ -0,0 +1,32 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by Felix Htoo on 18/05/2025.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let scrum: DailyScrum
+    var body: some View {
+        VStack (alignment: .leading) {
+            Text(scrum.title)
+                .font(.headline)
+            Spacer()
+            HStack () {
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+                Spacer()
+                Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+            }
+        }
+    }
+}
+
+#Preview(traits: .fixedLayout(width: 400, height: 60)) {
+    let scrum = DailyScrum.sampleData[0]
+    CardView(scrum: scrum)
+        .background(scrum.theme.mainColor)
+}
+
+
---
+++ Scrumdinger/Models/DailyScrum.swift
@@ -0,0 +1,16 @@
+//
+//  DailyScrum.swift
+//  Scrumdinger
+//
+//  Created by Felix Htoo on 18/05/2025.
+//
+
+import Foundation
+import ThemeKit
+
+struct DailyScrum {
+    var title: String
+    var attendees: [String]
+    var lengthInMinutes: Int
+    var theme: Theme
+}
